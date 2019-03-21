bd24639d84055fb6e91dc4a3c4afc4930f76269f
Christos Mizikakis <cmizikakis@gmail.com>
2019-03-21 10:18:23 +0200

    Connect to Db, Create SetDataInDAtanbase Controller and View

--- MyRegiserAppWithCRUD/App_Start/RouteConfig.cs
+++ MyRegiserAppWithCRUD/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MyRegiserAppWithCRUD
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Register", action = "SetDataInDatabase", id = UrlParameter.Optional }
             );
         }
     }
---
+++ MyRegiserAppWithCRUD/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using MyRegiserAppWithCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyRegiserAppWithCRUD.Controllers
+{
+    public class RegisterController : Controller
+    {
+        MyRegisterAppWithCRUDEntitiesConn db = new MyRegisterAppWithCRUDEntitiesConn();
+        // GET: Register
+        public ActionResult SetDataInDatabase()
+        {
+            return View();
+        }
+    }
+}
